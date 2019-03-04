2eb5d717fd9958bf0bf4fcd9d287e91ce425adb6
Kostyan1996 <meshavkin1996@gmail.com>
2019-03-04 03:31:51 +0300

    Added extraction of phone and mask applying functions

--- rxmask.js
+++ rxmask.js
@@ -1,25 +1,46 @@
+// TODO: Add HTML to test
+// TODO: Add tests and try to crash it in every possible way
+// TODO: Transform it into a module
+
+// TODO: Add just number and just letter support
 let maskSymb = '*';
 let mask = '+7 (***) ***-**-**';
+let phoneInput = '1';
 
-function maskToRegex(maskSymbol, mask) {
+function maskToRegex(mask, maskSymbol) {
   let regexStr = RegExp(`(\\${maskSymbol}+)`);
   let regexArr = mask.split(regexStr);
   if (regexArr[regexArr.length - 1].length == 0) regexArr.slice(-1);
   regexArr = regexArr.map((el) => {
     if (el.indexOf(maskSymbol) != -1) {
-      return `(${el.replace(RegExp('\\' + maskSymbol, 'g'), '.')})`;
+      return `(${el.replace(RegExp('\\' + maskSymbol, 'g'), '.?')})`;
     } else {
-      return `(?:${el.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})`;
+      return `(?:${el.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})?`;
     }
   });
   return RegExp(regexArr.join(''));
 }
 
-function matchRawString() {
-  
+function getRawPhone(phoneInput, mask) {
+  let rawArr = phoneInput.match(mask);
+  if (rawArr) {
+    return rawArr.slice(1).join('');
+  } else {
+    return '';
+  }
 }
 
-let maskStr = maskToRegex(maskSymb, mask);
+function applyMask(rawPhone, mask, maskSymb) {
+  rawPhone = [...rawPhone];
+  return [...mask].map((el) => {
+    return (el == maskSymb) ? rawPhone.shift() : el;
+  }).join('');
+}
+
+let maskStr = maskToRegex(mask, maskSymb);
+let rawPhone = getRawPhone(phoneInput, maskStr);
+let resultStr = applyMask(rawPhone, mask, maskSymb);
 
 console.log(maskStr);
-console.log('+7 (123) 456-78-90'.match(maskStr));
+console.log(rawPhone);
+console.log(resultStr);
