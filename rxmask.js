f64b7373c5545cb7da4dffc4c52b3bb8bb90f505
Kostyan1996 <meshavkin1996@gmail.com>
2019-03-12 01:45:58 +0300

    Added JSDoc for inputMask function, some error handlers and allowed to type custom regex for mask symbol

--- rxmask.js
+++ rxmask.js
@@ -7,16 +7,16 @@
 // TODO: Check for unicode support
 // TODO: Fixed start of mask
 
-function maskToRegex(mask, maskSymbol) {
+function maskToRegex(mask, maskSymbol, customRegex) {
   let regexArr = mask.split(RegExp(`(${regexLiteral(maskSymbol)}+)`));
   regexArr = regexArr.filter((el) => el);
-  regexArr = regexArr.map((el) => replaceSymbol(el, maskSymbol));
+  regexArr = regexArr.map((el) => replaceSymbol(el, maskSymbol, customRegex));
   return RegExp(regexArr.join(''));
 }
 
-function replaceSymbol(el, maskSymbol) {
+function replaceSymbol(el, maskSymbol, customRegex) {
   if (el.indexOf(maskSymbol) != -1) {
-    return `(\\d{0,${el.length}})`; // ! Return to .
+    return `(${customRegex}{0,${el.length}})`;
   } else {
     return [...el].map((ele) => `(?:${regexLiteral(ele)})?`).join('');
   }
@@ -35,10 +35,10 @@ function getRawInput(phoneInput, mask) {
   }
 }
 
-function applyMask([...rawInput], [...mask], maskSymb) {
+function applyMask([...rawInput], [...mask], maskSymbol) {
   return mask.map((el) => {
     if (rawInput.length < 1) return ''; // TODO: Let user decide - keep rest of mask or not
-    return (el == maskSymb) ? rawInput.shift() : el;
+    return (el == maskSymbol) ? rawInput.shift() : el;
   }).join('');
 }
 
@@ -47,14 +47,38 @@ function setCursorPos(prevPos, mask, maskSymbol) {
   return (rawSymbolPos == -1) ? prevPos : rawSymbolPos + 1;
 }
 
-function inputMask(element, mask, maskSymbol = '*') {
-  // TODO: function for removing symbol on cursor position if value.length > mask.length
-  const position = element.selectionStart;
-  let maskStr = maskToRegex(mask, maskSymbol);
-  console.log(maskStr);
-  const rawInput = getRawInput(element.value, maskStr);
-  console.log(rawInput);
-  const resultStr = applyMask(rawInput, mask, maskSymbol);
-  element.value = resultStr;
-  element.selectionEnd = setCursorPos(position, mask, maskSymbol); // ! Sometimes it selects one element, fix
+/**
+ * Use this for <input> element, pass this into "oninput" event
+ * @example
+ * /// Will replace all * symbols only with numbers from 1 to 5
+ * <input oninput="inputMask(this, '+7 (***) ***-**-**', '*', /[1-5]/)"></input>
+ *
+ * @param {Object} element Pass {this} into this parameter, it operates provided input
+ * @param {String} mask Mask string itself
+ * @param {String} [maskSymbol] Symbol in mask that will be interpreted as symbol to replace with typed value
+ * @param {RegExp} [customRegex] Regex expression to be evaluated upon typed value
+ */
+function inputMask(element, mask, maskSymbol = '*', customRegex = /./) {
+  try {
+    mask += ''; maskSymbol += ''; // Guard for non string values
+
+    if (!(customRegex instanceof RegExp)) throw new Error('Regex object was not provided!');
+    if (!mask.length) throw new Error('Wrong mask provided!');
+    if (!maskSymbol.length) throw new Error('Wrong mask symbol provided');
+
+    // Format stuff
+    customRegex = customRegex.source;
+    maskSymbol = maskSymbol[0];
+
+    const position = element.selectionStart;
+
+    const maskStr = maskToRegex(mask, maskSymbol, customRegex);
+    const rawInput = getRawInput(element.value, maskStr);
+    const resultStr = applyMask(rawInput, mask, maskSymbol);
+    element.value = resultStr;
+
+    element.selectionEnd = setCursorPos(position, mask, maskSymbol); // ! Sometimes it selects one element, fix
+  } catch (e) {
+    console.log(e);
+  }
 }
