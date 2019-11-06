1e44bd28f5c378a5c62d4414351711b3f7ad3f93
Kostyan1996 <meshavkin1996@gmail.com>
2019-11-07 02:48:49 +0300

    Prototype works, format code and add rest of features

--- src/rxmask.js
+++ src/rxmask.js
@@ -11,12 +11,41 @@ function Input(input) {
     this.input = input;
     this.prevValue = '';
     input.oninput = () => {
-        parseMask(input.getAttribute('mask'), input.getAttribute('maskSymbol'));
-        input.value = input.value + '3';
+        const mask = input.getAttribute('mask');
+        const maskSymbol = input.getAttribute('maskSymbol');
+        let cursorPos = input.selectionStart;
+        const parsedMask = parseMask(mask, maskSymbol);
+        let tempParsedMask = parsedMask;
+        let rawValue = input.value.split('').reduce((acc, c) => {
+            if (tempParsedMask[0] === c) {
+                tempParsedMask = tempParsedMask.slice(1);
+                return acc;
+            }
+            return acc + c;
+        }, '');
+        let output = '';
+        const splMask = mask.split('');
+        for (let i = 0; i < splMask.length; i++) {
+            if (rawValue.length === 0)
+                break;
+            if (splMask[i] === maskSymbol) {
+                output += rawValue[0];
+                rawValue = rawValue.slice(1);
+            }
+            else {
+                output += splMask[i];
+                cursorPos++;
+            }
+        }
+        input.value = output;
+        input.setSelectionRange(cursorPos, cursorPos);
         // End
         this.prevValue = input.value;
     };
 }
-function parseMask(mask, maskSymbol) {
-    console.log(mask, maskSymbol);
+function parseMask(mask, maskSymbol = '*') {
+    return mask.replace(new RegExp(regexLiteral(maskSymbol), 'g'), '');
+}
+function regexLiteral(rx) {
+    return rx.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
