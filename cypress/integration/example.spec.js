e473e9fbbf04d4a7571678b689448e39718c2777
Kostyan1996 <meshavkin1996@gmail.com>
2019-11-20 00:04:01 +0300

    set up cypress

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,4 +1,4 @@
-/* eslint-disable spaced-comment */
+// eslint-disable-next-line spaced-comment
 /// <reference types="cypress" />
 
 before(() => {
@@ -13,23 +13,53 @@ describe('Simple input', () => {
     cy.get('input.simple')
       .type('123')
       .should('have.value', '123')
+      .and('have.prop', 'selectionStart', 3)
+      .and('have.prop', 'selectionEnd', 3)
       .type('4')
       .should('have.value', '123-4')
+      .and('have.prop', 'selectionStart', 5)
+      .and('have.prop', 'selectionEnd', 5)
       .type('5')
       .should('have.value', '123-45')
+      .and('have.prop', 'selectionStart', 6)
+      .and('have.prop', 'selectionEnd', 6)
       .type('6')
       .should('have.value', '123-45-6')
+      .and('have.prop', 'selectionStart', 8)
+      .and('have.prop', 'selectionEnd', 8)
       .type('7')
-      .should('have.value', '123-45-67');
+      .should('have.value', '123-45-67')
+      .and('have.prop', 'selectionStart', 9)
+      .and('have.prop', 'selectionEnd', 9);
   });
 
   it('should not allow to add more symbols after mask is complete', () => {
     cy.get('input.simple')
-      .type('123-45-67')
+      .type('1234567')
       .should('have.value', '123-45-67')
+      .and('have.prop', 'selectionStart', 9)
+      .and('have.prop', 'selectionEnd', 9)
       .type('1')
       .should('have.value', '123-45-67')
+      .and('have.prop', 'selectionStart', 9)
+      .and('have.prop', 'selectionEnd', 9)
       .type('{leftarrow}1')
-      .should('have.value', '123-45-67');
+      .should('have.value', '123-45-67')
+      .and('have.prop', 'selectionStart', 8)
+      .and('have.prop', 'selectionEnd', 8);
+  });
+
+  it('should correctly parse pasted values', () => {
+    cy.get('input.simple')
+      .invoke('val', '123')
+      .trigger('input')
+      .should('have.value', '123')
+      .and('have.prop', 'selectionStart', 3)
+      .and('have.prop', 'selectionEnd', 3)
+      .type('{leftarrow}{leftarrow}')
+      .invoke('val', '12345')
+      .trigger('input')
+      .and('have.prop', 'selectionStart', 3)
+      .and('have.prop', 'selectionEnd', 3); // paste doesn't work
   });
 });
