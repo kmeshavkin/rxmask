a7f07186b84cb6f1e0aef36bdcb8f177f0f0f0b0
Kostyan1996 <meshavkin1996@gmail.com>
2020-02-02 20:18:21 +0300

    Changed build process with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,30 @@
-const webpack = require('webpack');
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: './src/rxmask.js',
+  entry: {
+    rxmask: './src/rxmask.ts',
+    'rxmask.min': './src/rxmask.ts'
+  },
   output: {
     path: path.resolve(__dirname, 'src'),
-    filename: 'rxmask.min.js'
+    filename: '[name].js'
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        include: /\.min\.js$/
+      })
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        include: /src/
+      }
+    ]
   },
   mode: 'production'
 };
